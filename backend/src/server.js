c2edaf1bf74c4d4388ee1b9d15fd2dc6be541e8f
Viktoriia <viktoriia.svisch@devit-team.com>
2020-12-13 03:45:14 +0000

    Adicionando url de conexão com banco

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const server = express();
+mongoose.connect('mongodb+srv:
+  useNewUrlParser: true
+});
+server.use(express.json);
 server.listen(3333);
