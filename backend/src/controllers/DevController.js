70499b0283ca36b40d905d4c432e3913b5721c2e
Viktoriia <viktoriia.svisch@devit-team.com>
2020-12-21 15:34:19 +0000

    Inserindo Dev de acordo com requisição do github

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,8 +1,16 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 module.exports = {
   async store(req, res) {
     const { username } = req.body;
     const response = await axios.get(`https:
-    return res.json(response.data);
+    const { name, bio, avatar_url: avatar } = response.data;
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    });
+    return res.json(dev);
   }
 };
