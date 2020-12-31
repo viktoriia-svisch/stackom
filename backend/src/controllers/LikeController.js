1ffa0a67865b86a1b4ec6af93907394be9b23cdd
Viktoriia <viktoriia.svisch@devit-team.com>
2020-12-31 00:06:19 +0000

    Implementando atribuição de um like

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,5 +1,15 @@
 const Dev = require('../models/Dev');
 module.exports = {
-  store() {
+  async store (req, res) {
+    const { devId } = req.params;
+    const { user } = req.headers;
+    const loggedUser = await Dev.findById(user);
+    const targetUser = await Dev.findById(devId);
+    if (!targetUser) {
+      return res.status(400).json({ error: 'Dev not exists' });
+    }
+    loggedUser.likes.push(targetUser._id);
+    await loggedUser.save();
+    return res.json(loggedUser);
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
 const router = express.Router();
 router.post('/devs', DevController.store);
+router.post('/devs/:devId/likes', LikeController.store)
 module.exports = router;
