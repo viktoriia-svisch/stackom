27db5b358c4d3f16e9bf95413fe3a1f4c522a988
Viktoriia <viktoriia.svisch@devit-team.com>
2021-01-11 21:55:13 +0000

    Adicionando biblioteca cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const server = express();
 mongoose.connect('mongodb+srv:
   useNewUrlParser: true
 });
+server.use(cors());
 server.use(express.json);
 server.use(routes);
 server.listen(3333);
