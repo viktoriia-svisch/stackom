d792e6241fbcc35f94a8d746ab46873d675e6bf5
Viktoriia <viktoriia.svisch@devit-team.com>
2021-01-18 19:45:29 +0000

    Limpando pasta do frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https:
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 export default App;
