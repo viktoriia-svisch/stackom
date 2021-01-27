2ecf2d7991443c4397fd71be6f59611730727785
Viktoriia <viktoriia.svisch@devit-team.com>
2021-01-27 05:50:44 +0000

    Adicionando container ao login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,6 +3,8 @@ import './Login.css';
 import logo from '../assets/logo.svg';
 export default function Login() {
   return (
-    <img src={logo} alt="Tindev"/>
+    <div className="login-container">
+      <img src={logo} alt="Tindev"/>
+    </div>
   );
 }
