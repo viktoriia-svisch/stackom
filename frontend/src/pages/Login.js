b8fa4e9b9207a2f35911921f3e4c0a1eb6ba00ac
Viktoriia <viktoriia.svisch@devit-team.com>
2021-01-29 12:48:31 +0000

    Adicionando input e button do login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,7 +4,11 @@ import logo from '../assets/logo.svg';
 export default function Login() {
   return (
     <div className="login-container">
-      <img src={logo} alt="Tindev"/>
+      <form>
+        <img src={logo} alt="Tindev"/>
+        <input placeholder="Digite seu usuário do Github" />
+        <button type="submit"></button>
+      </form>
     </div>
   );
 }
