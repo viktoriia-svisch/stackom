13dfdf3e2d778c68fb38900ca0d3ff06e261f0ca
Viktoriia <viktoriia.svisch@devit-team.com>
2021-02-14 13:56:42 +0000

    Recebendo dados do input e lidando com submit do form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import logo from '../assets/logo.svg';
 export default function Login() {
+  const [ username, setUsername ] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(username);
+  }
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev"/>
-        <input placeholder="Digite seu usuário do Github" />
+        <input
+          placeholder="Digite seu usuário do Github"
+          value={username}
+          onChange={ (e) => setUsername(e.target.value) }
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
