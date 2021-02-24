0236367fbb7829e6fc53c08eca1b1a8c7c19908d
Viktoriia <viktoriia.svisch@devit-team.com>
2021-02-24 00:18:00 +0000

    Adicionando base para a visualização dos devs

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,30 @@
 import React from 'react';
-export default function Main() {
+import './main.css';
+import api from '../services/api';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+export default function Main({ match }) {
   return (
-    <h1>Hello World</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <footer>
+            <strong></strong>
+            <p></p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
