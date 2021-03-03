b4604dc4c39fad38e2e873d324061c9448e4d9f1
Viktoriia <viktoriia.svisch@devit-team.com>
2021-03-03 09:07:55 +0000

    Implementando handle dos botões de like e dislike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -17,22 +17,26 @@ export default function Main({ match }) {
     }
     loadUsers();
   }, [ match.params.id ]);
+  async function handleLike(id) {
+  }
+  async function handleDislike(id) {
+  }
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev"/>
       <ul>
         { users.map((user) => (
-          <li>
+          <li key={user._id}>
             <img src={user.avatar} alt={user.name} />
             <footer>
               <strong>{user.name}</strong>
               <p>{user.bio}</p>
             </footer>
             <div className="buttons">
-              <button type="button">
+              <button type="button" onClick={() => handleDislike(user._id)}>
                 <img src={dislike} alt="Dislike" />
               </button>
-              <button type="button">
+              <button type="button" onClick={() => handleLike(user._id)}>
                 <img src={like} alt="Like" />
               </button>
             </div>
