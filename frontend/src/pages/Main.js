9cd8f59d87be1c019d55fb20cf5c80dda3d28e01
Viktoriia <viktoriia.svisch@devit-team.com>
2021-03-05 12:54:23 +0000

    Atualizando devs ao dar like ou dislike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './main.css';
+import './Main.css';
 import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
@@ -18,14 +18,27 @@ export default function Main({ match }) {
     loadUsers();
   }, [ match.params.id ]);
   async function handleLike(id) {
+    await api.post(`/${id}/like`, null, {
+      headers: {
+        user: match.params.id
+      },
+    });
+    setUsers(users.filter((user) => user._id !== id));
   }
   async function handleDislike(id) {
+    await api.post(`/${id}/dislike`, null, {
+      headers: {
+        user: match.params.id
+      },
+    });
+    setUsers(users.filter((user) => user._id !== id));
   }
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev"/>
-      <ul>
-        { users.map((user) => (
+      { users.length > 0 ? (
+        <ul>
+          { users.map((user) => (
           <li key={user._id}>
             <img src={user.avatar} alt={user.name} />
             <footer>
@@ -40,9 +53,11 @@ export default function Main({ match }) {
                 <img src={like} alt="Like" />
               </button>
             </div>
-          </li>
-        )) }
-      </ul>
+          </li>)) }
+        </ul>
+      ) : (
+        <div className="empty"> Acabou :( </div>
+      )}
     </div>
   );
 }
