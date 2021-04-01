109190b3ea2648d588aad55d7c6f2f09e90f7f3b
Viktoriia <viktoriia.svisch@devit-team.com>
2021-04-01 20:24:14 +0000

    Adicionando interface da tela de login

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Login from './pages/Login';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <Login />
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontWeight: 'bold',
-    color: '#fff',
-    fontSize: 20,
-  }
-});
