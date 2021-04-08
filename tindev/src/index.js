c8b93548ff2e6d95808638e8be4aa7a5f3597533
Viktoriia <viktoriia.svisch@devit-team.com>
2021-04-08 02:49:40 +0000

    Adicionando routes e arquivo da main screen

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Login from './pages/Login';
+import Routes from './routes';
 export default function App() {
   return (
-    <Login />
+    <Routes />
   );
 };
