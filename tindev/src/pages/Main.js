ef6b1f8964818386a0e838a0ca85ffef943b1e84
Viktoriia <viktoriia.svisch@devit-team.com>
2021-04-28 02:33:32 +0000

    Carregando devs da api

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Image, StyleSheet, View, Text,
   TouchableOpacity } from 'react-native';
 import api from '../services/api';
 import logo from '../assets/logo.png';
 import dislike from '../assets/dislike.png';
 import like from '../assets/like.png';
-export default function Main() {
+export default function Main({ navigation }) {
+  const id = navigation.getParam('user');
+  const [ users, setUsers ] = useState([]);
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: id,
+        }
+      });
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [ id ]);
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/like`, null, {
+      headers: {
+        user: id
+      },
+    });
+    setUsers(users.filter((user) => user._id !== id));
+  }
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislike`, null, {
+      headers: {
+        user: id
+      },
+    });
+    setUsers(users.filter((user) => user._id !== id));
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={logo} />
       <View style={styles.cardsContainer}>
-        <View style={[ styles.card, { zIndex: 3 }]}>
-          <Image style={styles.avatar} source={{ uri: "https:
-          <View style={styles.footer}>
-            <Text style={styles.name}>Welbert Hime</Text>
-            <Text style={styles.bio} numberOfLines={3}>Técnico em Redes de Computadores, discente do curso de Engenharia de Computação, Diretor Presidente da Pampec Jr.</Text>
+        { users.length === 0
+          ? <Text style={styles.empty}>Acabou :(</Text>
+          : (users.map((user,index) => (
+          <View key={user._id} style={[ styles.card, { zIndex: users.length - index }]}>
+            <Image style={styles.avatar} source={{ uri: user.avatar }} />
+            <View style={styles.footer}>
+              <Text style={styles.name}>{user.name}</Text>
+              <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+            </View>
           </View>
-        </View>
-        <View style={[ styles.card, { zIndex: 2 }]}>
-          <Image style={styles.avatar} source={{ uri: "https:
-          <View style={styles.footer}>
-            <Text style={styles.name}>Welbert Hime</Text>
-            <Text style={styles.bio} numberOfLines={3}>Técnico em Redes de Computadores, discente do curso de Engenharia de Computação, Diretor Presidente da Pampec Jr.</Text>
-          </View>
-        </View>
-        <View style={[ styles.card, { zIndex: 1 }]}>
-          <Image style={styles.avatar} source={{ uri: "https:
-          <View style={styles.footer}>
-            <Text style={styles.name}>Welbert Hime</Text>
-            <Text style={styles.bio} numberOfLines={3}>Técnico em Redes de Computadores, discente do curso de Engenharia de Computação, Diretor Presidente da Pampec Jr.</Text>
-          </View>
-        </View>
+        ))) }
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
@@ -52,6 +71,12 @@ const styles = StyleSheet.create({
   },
   logo: {
     marginTop: 30,
+  },
+  empty: {
+    alignSelf: 'center',
+    color: '#999',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
   cardsContainer: {
     flex: 1,
@@ -110,5 +135,5 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2,
     },
-  }
+  },
 });
