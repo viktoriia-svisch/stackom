9725f521fff196216eb3f5e278c9c7aef897770e
Viktoriia <viktoriia.svisch@devit-team.com>
2021-05-07 09:58:21 +0000

    Ocultando botoes de like e dislike

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -60,14 +60,16 @@ export default function Main({ navigation }) {
           </View>
         ))) }
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleDislike}>
-          <Image source={dislike} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleLike}>
-          <Image source={like} />
-        </TouchableOpacity>
-      </View>
+      { users.length > 0 && (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleDislike}>
+            <Image source={dislike} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleLike}>
+            <Image source={like} />
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
