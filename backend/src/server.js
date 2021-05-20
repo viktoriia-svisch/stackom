e5763d3bb073cd96a7a44a48b605be44dcfba2f3
Viktoriia <viktoriia.svisch@devit-team.com>
2021-05-20 14:20:14 +0000

    Passando argumentos dos usuários conectados as requisições

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,11 @@ io.on('connect', (socket) => {
 mongoose.connect('mongodb+srv:
   useNewUrlParser: true
 });
+app.use((req,res,next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(routes);
